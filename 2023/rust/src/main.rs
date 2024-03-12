3f31d446a80909c6293c3273e2342a9db0ee5e72
Vineet Kulkarni <vineet.kulkarni8@gmail.com>
2024-03-12 10:06:36 +0530

    added rust project for 2023

---
+++ 2023/rust/src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    guess = guess + "12";
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+}
